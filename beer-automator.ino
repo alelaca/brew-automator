7f3752cf844fe7d52c2e988fd25044b4369e01ec
Alejandro Lacassy <ale.lacassy@gmail.com>
2021-07-16 14:35:39 -0300

    Feature/2004 led screen (#5)

--- beer-automator.ino
+++ beer-automator.ino
@@ -1,11 +1,15 @@
-#include <OneWire.h> 
+#include <Wire.h>
+#include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
 class Alarm {
   private:
     int pin;
   public:
+    Alarm(){}
+
     Alarm(int pin) {
       this->pin = pin;
       pinMode(pin, OUTPUT);
@@ -27,13 +31,15 @@ class TemperatureSensor {
     OneWire oneWire;
     DallasTemperature sensors;
   public:
+    TemperatureSensor() {}
+
     TemperatureSensor(int pin) {
       this->pin = pin;
-      
+
       oneWire = OneWire(pin);
       sensors = DallasTemperature(&oneWire);
-      
-      sensors.begin(); 
+
+      sensors.begin();
     }
 
   int getTemperature() {
@@ -52,19 +58,29 @@ class Joystick {
     int deadZoneY = 500;
     int selectOneZone = 23;
 
-    bool selected = false;
+    bool selectedX = false;
+    bool selectedY = false;
 
-    void handleSelectionTypes(int x, int y) {
+    void handleSelectionType(int x, int y) {
       if (y < 1023 / 2 + deadZoneY / 2 && y > 1023 / 2 - deadZoneY / 2) {
-        selected = false;
+        selectedY = false;
+      } else if (x < 1023 / 2 + deadZoneX / 2 && x > 1023 / 2 - deadZoneX / 2) {
+        selectedX = false;
+      } else if (y >= 1023 - selectOneZone || y <= 0 + selectOneZone) {
+        selectedY = false;
+      } else if (x >= 1023 - selectOneZone || x <= 0 + selectOneZone) {
+        selectedX = false;
       }
     }
+
   public:
     String LEFT_MOVEMENT = "L";
     String RIGHT_MOVEMENT = "R";
     String UP_MOVEMENT = "U";
     String DOWN_MOVEMENT = "D";
     String PRESS_MOVEMENT = "P";
+
+    Joystick() {}
 
     Joystick(int xPin, int yPin, int swPin) {
       this->xPin = xPin;
@@ -81,32 +97,32 @@ class Joystick {
       int y = analogRead(yPin);
       int swState = digitalRead(swPin);
 
-      if (selected) {
-        handleSelectionTypes(x, y);
+      if (selectedX || selectedY) {
+        handleSelectionType(x, y);
         return "";
       }
 
       if (x > (1023 / 2 + deadZoneX / 2)) {
         if (x < 1023 - selectOneZone) {
-          selected = true;
+          selectedX = true;
         }
         return RIGHT_MOVEMENT;
       }
       if (x < (1023 / 2 - deadZoneX / 2)) {
         if (x > 0 + selectOneZone) {
-          selected = true;
+          selectedX = true;
         }
         return LEFT_MOVEMENT;
       }
       if (y > (1023 / 2 + deadZoneY / 2)) {
         if (y < 1023 - selectOneZone) {
-          selected = true;
+          selectedY = true;
         }
         return DOWN_MOVEMENT;
       }
       if (y < (1023 / 2 - deadZoneY / 2)) {
         if (y > 0 + selectOneZone) {
-          selected = true;
+          selectedY = true;
         }
         return UP_MOVEMENT;
       }
@@ -125,24 +141,26 @@ class Button {
     int lastState = LOW;
     int pin;
   public:
+    Button() {}
+
     Button(byte pin) {
       this->pin = pin;
       pinMode(pin, INPUT);
     }
-  
+
     void update() {
       int state = digitalRead(pin);
-      
+
       if (state == HIGH && lastState == LOW) {
         pressed = 1 - pressed;
         delay(20);
       } else {
         pressed = 0;
       }
-      
+
       lastState = state;
     }
-  
+
     bool isPressed() {
       update();
       return (pressed == HIGH);
@@ -150,28 +168,44 @@ class Button {
 };
 
 class ScreenHandler {
-  private: 
-    LiquidCrystal screen = LiquidCrystal(12, 11, 5, 4, 3, 2);
+  private:
+    LiquidCrystal_I2C screen = LiquidCrystal_I2C(0x27, 20, 4);
 
   public:
     ScreenHandler() {
-      screen.begin(16, 2);
     }
 
-    ScreenHandler(int width, int height) {
-      screen.begin(width, height);
+    void initializeScreen() {
+      screen.init();
+      screen.backlight();
     }
 
-    void show(String upperMessage, String bottomMessage) {
-      upperMessage += "          ";
-      bottomMessage += "          ";
-      
+    void show(String firstLine, String secondLine, String thirdLine, String forthLine) {
+      /*String fillLine = "";
+      firstLine += fillLine;
+      secondLine += fillLine;
+      thirdLine += fillLine;
+      forthLine += fillLine;*/
+
       screen.setCursor(0, 0);
-      screen.print(upperMessage);
+      screen.print(firstLine);
       screen.setCursor(0, 1);
-      screen.print(bottomMessage);
+      screen.print(secondLine);
+      screen.setCursor(0, 2);
+      screen.print(thirdLine);
+      screen.setCursor(0, 3);
+      screen.print(forthLine);
     }
-  
+
+    void clearAndShow(String firstLine, String secondLine, String thirdLine, String forthLine) {
+      screen.clear();
+      show(firstLine, secondLine, thirdLine, forthLine);
+    }
+
+    void showInCursor(String msg, int row, int col) {
+      screen.setCursor(col, row);
+      screen.print(msg);
+    }
 };
 
 class MashProcessor {
@@ -190,16 +224,11 @@ class MashProcessor {
     int manualModeSwitch = LOW;
     int waterPumpSwitch = LOW;
     int waterPumpHeatSwitch = LOW;
-  
-   Joystick joystick = Joystick(0, 1, 8);
-   TemperatureSensor temperatureSensor = TemperatureSensor(7);
-   Alarm alarm = Alarm(13);
 
     ScreenHandler screenHandler;
-
-    const int MENU_ACTUAL_MASHING_DISPLAYED = 1;
-    const int MENU_SET_MASHING_DISPLAYED = 2;
-    int currentMashingMenu = MENU_ACTUAL_MASHING_DISPLAYED;
+    TemperatureSensor temperatureSensor;
+    Joystick joystick;
+    Alarm alarm;
 
     const int CONFIG_TEMP_ID = 1;
     const int CONFIG_TIME_ID = 2;
@@ -209,38 +238,70 @@ class MashProcessor {
     bool alarmActivated = false;
 
     void displayMashSelectionMenu() {
-      String mashTemperatureMsg = "T=" + String(selectedMashTemperature) + "C t=" + String(selectedMashTime) + "m";
-      screenHandler.show("Mash config", mashTemperatureMsg);
+      String mashTemperatureMsg = " temp = " + String(selectedMashTemperature) + " C";
+      String mashTimeMsg = " time = " + String(selectedMashTime) + " min";
+      screenHandler.clearAndShow("Mashing config", "", mashTemperatureMsg, mashTimeMsg);
+    }
+
+    void displayMashSelectionUpdatingValues() {
+      String mashTemperatureMsg = String(selectedMashTemperature) + " C";
+      String mashTimeMsg = String(selectedMashTime) + " min";
+      screenHandler.showInCursor(mashTemperatureMsg, 2, 8);
+      screenHandler.showInCursor(mashTimeMsg, 3, 8);
     }
 
     void displayRecirculationSelectionMenu() {
-      String recirculationTimeMsg ="t=" + String(selectedRecirculationTime) + "m";
-      screenHandler.show("Recirc. config", recirculationTimeMsg);
+      String recirculationTimeMsg = " time = " + String(selectedRecirculationTime) + " min";
+      screenHandler.clearAndShow("Recirculation config", "", recirculationTimeMsg, "");
+    }
+
+    void displayRecirculationSelectionUpdatingValues(){
+      String recirculationTimeMsg = String(selectedRecirculationTime) + " min";
+      screenHandler.showInCursor(recirculationTimeMsg, 2, 8);
     }
 
     void displayConfirmConfigMenu() {
-      String configMsg = "M=" + String(selectedMashTemperature) + "C/" + String(selectedMashTime) + "m R=" + String(selectedRecirculationTime) + "m";
-      screenHandler.show("Confirm config", configMsg);
+      String mashSettingMsg = "Mash set " + String(selectedMashTemperature) + "C " + String(selectedMashTime) + "min";
+      String recirculateSettingMsg = "Rercirc set " + String(selectedRecirculationTime) + "min";
+      screenHandler.clearAndShow("Confirm settings", "", mashSettingMsg, recirculateSettingMsg);
     }
 
-    void displayMashingMenu(int temp, int timeInSeg) {
-      String mashMsg = "T=" + String(temp) + "C t=" + getTimeFormat(timeInSeg);
-      screenHandler.show("Mashing ...", mashMsg);
+    void displayMashingMenu() {
+      String formattedSelectedTime = selectedMashTime<10? "0" + String(selectedMashTime): String(selectedMashTime);
+      String mashingTempMsg = " temp  " + String(selectedMashTemperature) + "C | " + String(actualMashTemperature) + "C";
+      String mashingTimeMsg = " time  " + formattedSelectedTime + "m | " + getTimeFormat(actualMashTime);
+      screenHandler.clearAndShow("Mashing", "       set | actual", mashingTempMsg, mashingTimeMsg);
+    }
+
+    void displayMashingUpdatingValues() {
+      String mashingTempMsg = String(actualMashTemperature) + "C";
+      String mashingTimeMsg = getTimeFormat(actualMashTime);
+      screenHandler.showInCursor(mashingTempMsg, 2, 13);
+      screenHandler.showInCursor(mashingTimeMsg, 3, 13);
     }
 
     void displayRecirculatingMenu() {
-      String recircMsg = "t=" + getTimeFormat(actualRecirculationTime) + " T=" + String(actualMashTemperature);
-      screenHandler.show("Recirculating", recircMsg); 
+      String formattedSelectedTime = selectedRecirculationTime<10? "0" + String(selectedRecirculationTime): String(selectedRecirculationTime);
+      String recircTempMsg = " temp      | " + String(actualMashTemperature) + "C"; // FIXME change this variable to one for recirc
+      String recircTimeMsg = " time  " + formattedSelectedTime + "m | " + getTimeFormat(actualRecirculationTime);
+      screenHandler.clearAndShow("Recirculating", "       set | actual", recircTempMsg, recircTimeMsg);
+    }
+
+    void displayRecirculatingUpdatingValues(){
+      String recircTempMsg = String(actualMashTemperature) + "C"; // FIXME change this variable to one for recirc
+      String recircTimeMsg = getTimeFormat(actualRecirculationTime);
+      screenHandler.showInCursor(recircTempMsg, 2, 13);
+      screenHandler.showInCursor(recircTimeMsg, 3, 13);
     }
 
     void displayFinishMenu() {
-      screenHandler.show("Process finished", "Lets cook it!");
+      screenHandler.clearAndShow("Process finished", "", "Lets cook it!", "");
     }
 
     String getTimeFormat(int timeInSeg) {
       int min = timeInSeg / 60;
       int seg = timeInSeg - (min * 60);
-      
+
       String minStr = String(min);
       if (min < 10) {
         minStr = "0" + minStr;
@@ -265,7 +326,7 @@ class MashProcessor {
 
     void readMashSettings() {
       String movement = joystick.getMovement();
-      
+
       if (movement == joystick.RIGHT_MOVEMENT) {
          if (currentConfigToSelect == CONFIG_TEMP_ID) {
           currentConfig = &selectedMashTime;
@@ -279,42 +340,36 @@ class MashProcessor {
       }
       else if (movement == joystick.UP_MOVEMENT) {
         *currentConfig = *currentConfig + 1;
+        displayMashSelectionUpdatingValues();
       }
       else if (movement == joystick.DOWN_MOVEMENT) {
         *currentConfig = *currentConfig - 1;
+        displayMashSelectionUpdatingValues();
       }
     }
-  
+
   void readRecirculationSettings() {
     String movement = joystick.getMovement();
-    
+
     if (movement == joystick.UP_MOVEMENT) {
         selectedRecirculationTime++;
+        displayRecirculationSelectionUpdatingValues();
       }
       else if (movement == joystick.DOWN_MOVEMENT) {
         selectedRecirculationTime--;
+        displayRecirculationSelectionUpdatingValues();
       }
   }
 
   void processMash() {
     startingTime = millis();
-
+    displayMashingMenu();
+    
     while ((actualMashTime / 60) < selectedMashTime) {
       actualMashTime = (millis() - startingTime) / 1000;
       actualMashTemperature = temperatureSensor.getTemperature();
 
-      String movement = joystick.getMovement();
-      if (currentMashingMenu == MENU_SET_MASHING_DISPLAYED) {
-        displayMashingMenu(selectedMashTemperature, selectedMashTime*60);
-        if (movement == joystick.UP_MOVEMENT || movement == joystick.DOWN_MOVEMENT) {
-          currentMashingMenu = MENU_ACTUAL_MASHING_DISPLAYED;
-        }
-      } else {
-        displayMashingMenu(actualMashTemperature, actualMashTime);
-        if (movement == joystick.UP_MOVEMENT || movement == joystick.DOWN_MOVEMENT) {
-          currentMashingMenu = MENU_SET_MASHING_DISPLAYED;
-        }
-      }
+      displayMashingUpdatingValues();
 
       if (actualMashTemperature < selectedMashTemperature) {
         if (!alarmActivated) {
@@ -344,11 +399,13 @@ class MashProcessor {
 
   void processRecirculation() {
     startingTime = millis();
-
+    displayRecirculatingMenu();
+    
     while ((actualRecirculationTime / 60) < selectedRecirculationTime) {
       actualMashTemperature = temperatureSensor.getTemperature();
       actualRecirculationTime = (millis() - startingTime) / 1000;
-      displayRecirculatingMenu();
+      
+      displayRecirculatingUpdatingValues();
 
       if (waterPumpSwitch == LOW) {
         // alarm! water pump is off
@@ -361,8 +418,13 @@ class MashProcessor {
   }
 
   public:
-    MashProcessor(ScreenHandler screenHandler) {
+    MashProcessor() {}
+  
+    MashProcessor(ScreenHandler screenHandler, TemperatureSensor temperatureSensor, Joystick joystick, Alarm alarm) {
       this->screenHandler = screenHandler;
+      this->temperatureSensor = temperatureSensor;
+      this->joystick = joystick;
+      this->alarm = alarm;
     }
 
     void config() {
@@ -370,22 +432,22 @@ class MashProcessor {
         return;
       }
 
+      displayMashSelectionMenu();
       while (joystick.getMovement() != joystick.PRESS_MOVEMENT) {
-        displayMashSelectionMenu();
         readMashSettings();
       }
 
       delay(800);
 
+      displayRecirculationSelectionMenu();
       while (joystick.getMovement() != joystick.PRESS_MOVEMENT) {
-        displayRecirculationSelectionMenu();
         readRecirculationSettings();
       }
 
       delay(800);
 
+      displayConfirmConfigMenu();
       while (joystick.getMovement() != joystick.PRESS_MOVEMENT) {
-        displayConfirmConfigMenu();
       }
 
       configFinished = true;
@@ -406,18 +468,75 @@ class MashProcessor {
     }
 };
 
-ScreenHandler screenHandler = ScreenHandler(16, 2);
+TemperatureSensor temperatureSensor = TemperatureSensor(7);
+ScreenHandler screenHandler;
+Joystick joystick = Joystick(0, 1, 8);
+Alarm alarm = Alarm(13);
 
-MashProcessor mashProcessor = MashProcessor(screenHandler);
+MashProcessor* mashProcessor;
 
-Alarm alarma = Alarm(13);
+String menuItem1 = "1-Mash";
+String menuItem2 = "2-Cook";
+String menuItem3 = "3-Ferment";
+String menuItemSelector = " <--";
 
 void setup() {
-  screenHandler.show("Jarbier 1.0", "  Lets brew!");
+  screenHandler.initializeScreen();
+  screenHandler.show("    Jarbier 1.0", "", " Lets brew!", "");
   delay(4000);
+  mashProcessor = new MashProcessor(screenHandler, temperatureSensor, joystick, alarm);
 }
 
 void loop() {
-  mashProcessor.config();
-  mashProcessor.process();
+  printMenuOption(1);
+
+  bool optionChanged = false;
+  int selectedMenuOption = 1;
+  String joystickMovement = "";
+  
+  while (joystickMovement != joystick.PRESS_MOVEMENT) {
+    joystickMovement = joystick.getMovement();
+    if (joystickMovement == joystick.DOWN_MOVEMENT) {
+      selectedMenuOption++;
+      optionChanged = true;
+    }
+    else if (joystickMovement == joystick.UP_MOVEMENT) {
+      selectedMenuOption--;
+      optionChanged = true;
+    }
+
+    if (selectedMenuOption < 1) {
+      selectedMenuOption = 3;
+    } else if (selectedMenuOption > 3) {
+      selectedMenuOption = 1;
+    }
+
+    if (optionChanged) {
+      printMenuOption(selectedMenuOption);
+      optionChanged = false;
+    }
+  }
+
+  delay(800);
+
+  if (selectedMenuOption == 1) {
+    mashProcessor->config();
+    mashProcessor->process();
+  } else if (selectedMenuOption == 2) {
+    screenHandler.clearAndShow("Beer menu", "", "Option not ready yet", "");
+  } else if (selectedMenuOption == 3) {
+    screenHandler.clearAndShow("Beer menu", "", "Option not ready yet", "");
+  }
+
+  delay(2000);
+}
+
+void printMenuOption(int selected) {
+  if (selected == 1) {
+    screenHandler.clearAndShow("Beer menu", menuItem1 + menuItemSelector, menuItem2, menuItem3);
+  } else if (selected == 2) {
+    screenHandler.clearAndShow("Beer menu", menuItem1, menuItem2 + menuItemSelector, menuItem3);
+  } else if (selected == 3) {
+    screenHandler.clearAndShow("Beer menu", menuItem1, menuItem2, menuItem3 + menuItemSelector);
+  }
 }
